7a6405c3254b00c98cebcccb63140c9125f58c4a
THE-EGO-999-GT <theego999.me@gmail.com>
2026-09-13 17:31:00 +0600

    haydn: Include Leica Camera if exists
    Signed-off-by: THE-EGO-999-GT <theego999.me@gmail.com>

--- device.mk
+++ device.mk
@@ -80,8 +80,9 @@ $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/haydn/haydn-vendor.mk)
 
-# Call the proprietary setup for miui-camera
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-haydn/device.mk)
+# Call the Leica Camera setup
+$(call inherit-product-if-exists, vendor/xiaomi/haydn-miuicamera/products/miuicamera.mk)
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 # Call Gamebar
 $(call inherit-product, packages/apps/GameBar/gamebar.mk)
